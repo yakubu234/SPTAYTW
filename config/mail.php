32d55661468c3856a6aa5f0aa26a53d24ea880c2
yakubu abiola <yakubuabiola2003@gmail.com>
2026-09-16 19:07:51 +0100

    Add basic mail configuration

---
+++ config/mail.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'default' => env('MAIL_MAILER', 'log'),
+    'mailers' => [
+        'log' => [
+            'transport' => 'log',
+            'channel' => env('MAIL_LOG_CHANNEL'),
+        ],
+    ],
+    'from' => [
+        'address' => env('MAIL_FROM_ADDRESS', 'noreply@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Sporty Safe Engine'),
+    ],
+];
